e06e6f4f42db0863e29533f68720853b1df0a7f1
kaVII-x <ka52jiguli@yandex.ru>
2023-05-17 12:57:51 +0400

    version 1.1

--- src/main/java/sky/pro/calculator/controller/CalculatorController.java
+++ src/main/java/sky/pro/calculator/controller/CalculatorController.java
@@ -12,28 +12,31 @@ import sky.pro.calculator.service.CalculatorService;
 @RequestMapping("/calculator")
 public class CalculatorController {
     private final CalculatorService calculatorService;
-    private final String localNull = "Деление на 0 не возможно";
 
     private final String localEmpty = "Не должно быть пустых значений";
 
 
     public CalculatorController(CalculatorService calculatorService) {
+
         this.calculatorService = calculatorService;
     }
 
     @GetMapping
     public String hello() {
+
         return "Главная страница";
     }
 
     @GetMapping(path = "/calculator")
     public String calculatorHello() {
+
         return "Добро пожаловать в калькулятор";
     }
 
     @GetMapping(path = "/plus")
     public String calculatorPlus(@RequestParam("num1") String numFirst, @RequestParam("num2") String numTwo) {
         if (numFirst.isEmpty() || numTwo.isEmpty()) {
+
             return localEmpty;
         }
         return numFirst + " + " + numTwo + " = " + calculatorService.calculatorPlus(Integer.parseInt(numFirst), Integer.parseInt(numTwo));
@@ -42,6 +45,7 @@ public class CalculatorController {
     @GetMapping(path = "/minus")
     public String calculatorMinus(@RequestParam("num1") String numFirst, @RequestParam("num2") String numTwo) {
         if (numFirst.isEmpty() || numTwo.isEmpty()) {
+
             return localEmpty;
         }
         return numFirst + " - " + numTwo + " = " + calculatorService.calculatorMinus(Integer.parseInt(numFirst), Integer.parseInt(numTwo));
@@ -50,6 +54,7 @@ public class CalculatorController {
     @GetMapping(path = "/multiply")
     public String calculatorMultiply(@RequestParam("num1") String numFirst, @RequestParam("num2") String numTwo) {
         if (numFirst.isEmpty() || numTwo.isEmpty()) {
+
             return localEmpty;
         }
         return numFirst + " * " + numTwo + " = " + calculatorService.calculatorMultiply(Integer.parseInt(numFirst), Integer.parseInt(numTwo));
@@ -59,6 +64,7 @@ public class CalculatorController {
     @GetMapping(path = "/divide")
     public String calculatorDivide(@RequestParam("num1") String numFirst, @RequestParam("num2") String numTwo) {
         if (numFirst.isEmpty() || numTwo.isEmpty()) {
+
             return localEmpty;
         }
         return numFirst + " / " + numTwo + " = " + calculatorService.calculatorDivide(Integer.parseInt(numFirst), Integer.parseInt(numTwo));
